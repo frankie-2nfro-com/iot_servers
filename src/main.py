d2b8a234d9d4b98c638513c41135cb89c27b4d9d
frankie-2nfro-com <frankie@2nfro.com>
2022-11-25 18:37:02 +0000

    Add Dockerfile

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import sys
+
+print("Hello docker single stage image.....", sys.argv)
+
+# should be get argv to know the mqtt channel name
+# Loop the program after loading the model
+# When subscription message found - handle it and get model resutl
+# Publish result to the mqtt
